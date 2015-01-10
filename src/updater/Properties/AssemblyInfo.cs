d7e6cdfcf5c4f974a489fa40abcf905cb26a268d
icedream <icedream2k9@die-optimisten.net>
2015-01-10 05:33:38 +0100

    Add wrapper program to avoid Mono's dependency checks on startup.

--- src/updater/Properties/AssemblyInfo.cs
+++ src/updater/Properties/AssemblyInfo.cs
@@ -1,6 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+#if !NO_COMMANDLINE
 using CommandLine;
+#endif
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -13,8 +15,10 @@ using CommandLine;
 [assembly: AssemblyCopyright("© 2014-2015 Carl Kittelberger")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+#if !NO_COMMANDLINE
 [assembly: AssemblyLicense("This is free software. You may redistribute copies of it under the terms of the MIT License <http://www.opensource.org/licenses/mit-license.php>.")]
 [assembly: AssemblyUsage("Usage: citimp_upd.exe [options...] <outputdir>")]
+#endif
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
---
+++ src/wrapper/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using System.Reflection;
+
+// ReSharper disable once CheckNamespace
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var mainAsm = Assembly.Load("citizenmp_server_updater");
+
+        mainAsm.GetType("Costura.AssemblyLoader")
+            .GetMethod("Attach")
+            .Invoke(null, null);
+
+        mainAsm.GetType("CitizenMP.Server.Installer.Program")
+            .GetMethod("Main", BindingFlags.NonPublic | BindingFlags.Static)
+            .Invoke(null, new object[] {args});
+    }
+}
